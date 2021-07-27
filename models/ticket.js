d5f23ede9c42de2b601c628e7593904acc12d5c2
Billel Abbes <billel.abbes@etu.upmc.fr>
2021-07-27 19:38:00 +0200

    enlever les commentaires

--- models/ticket.js
+++ models/ticket.js
@@ -11,11 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-    //   models.Ticket.belongsTo(models.User, {
-    //     foreignKey: {
-    //         allowNull: false
-    //     }
-    // })
+      models.Ticket.belongsTo(models.User, {
+        foreignKey: {
+            allowNull: false
+        }
+    })
     }
   };
   Ticket.init({
